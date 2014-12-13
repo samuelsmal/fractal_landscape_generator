05c89621928a9d9d0f078b8b1b7dcd3db92aa721
Samuel von Baussnern <samuel.vonbaussnern@gmail.com>
2014-12-13 17:01:44 +0100

    adds square part and roughness first part

--- FractalLandscapeGenerator/src/flg/algorithm/DiamondSquare.java
+++ FractalLandscapeGenerator/src/flg/algorithm/DiamondSquare.java
@@ -1,14 +1,19 @@
 package flg.algorithm;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class DiamondSquare {
+	
+	
 	public static void applyDiamondSquare(double[][] points, double roughness) {
 		for (int i = points.length / 2; i > 0; --i) {
 			diamondStep(points, roughness, i);
-			//squareStep(points, roughness, i);
+			squareStep(points, roughness, i);
 			
-			roughness /= 2;
+			randomNumber(roughness);
+			
+			//roughness /= 2;
 		}
 	}
 	
@@ -37,13 +42,54 @@ public class DiamondSquare {
 					}
 				}
 				
-				points[i][j] = average(parentValues) + roughness;
+				points[i][j] = average(parentValues) + randomNumber(roughness);
 			}
 		}
 	}
 	
 	private static void squareStep(double[][] points, double roughness, int length) {
+		
+		for (int i=0;i<points.length;++i){
+			for(int j=0; j<points.length;++j){
+				ArrayList<Double> values = new ArrayList<Double>();
+			
+				if (i >= length) {
+					if (j >= length) {
+						values.add(points[i - length/2][j]);
+					}
+					
+					if (j + length < points.length) {
+						values.add(points[i][j + length/2]);
+					}
+				}
+				
+				if (i + length < points.length) {
+					if (j >= length) {
+						values.add(points[i][j - length/2]);
+					}
+					
+					if (j + length < points.length) {
+						values.add(points[i + length/2][j]);
+					}
+				}
+				
+				points[i][j] = average(values) + randomNumber(roughness);
+			}
+		}
 	}
+	
+	private static double randomNumber(double roughness){
+		
+		Random rand = new Random();
+		
+		if ((random <= roughness) && (random >= ((-1) * roughness))){
+			return random;
+		} else {
+		    return roughness/2;
+		}
+	}
+	
+	
 	
 	private static Double average(ArrayList<Double> l) {
 		Double sum = 0d;
