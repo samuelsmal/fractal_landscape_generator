63302b805857300ce4284ea2bd49c1ea88ab8afa
Roman Brenn <romanbrenn@gmail.com>
2014-12-13 20:58:47 +0100

    Adapt triangles to Point3d.

--- FractalLandscapeGenerator/src/flg/rendering/Triangle.java
+++ FractalLandscapeGenerator/src/flg/rendering/Triangle.java
@@ -1,14 +1,50 @@
 package flg.rendering;
 
+import javax.vecmath.Point3d;
+
 
 public class Triangle {
 
-    private double x, y, z;
+    private Point3d coordinate1;
+    private Point3d coordinate2;
+    private Point3d coordinate3;
 
-    public Triangle(double x, double y, double z) {
+
+    public Triangle(Point3d coordinate1, Point3d coordinate2, Point3d coordinate3) {
         super();
-        this.x = x;
-        this.y = y;
-        this.z = z;
+        this.coordinate1 = coordinate1;
+        this.coordinate2 = coordinate2;
+        this.coordinate3 = coordinate3;
     }
+
+
+    public Point3d getCoordinate1() {
+        return coordinate1;
+    }
+
+
+    public void setCoordinate1(Point3d coordinate1) {
+        this.coordinate1 = coordinate1;
+    }
+
+
+    public Point3d getCoordinate2() {
+        return coordinate2;
+    }
+
+
+    public void setCoordinate2(Point3d coordinate2) {
+        this.coordinate2 = coordinate2;
+    }
+
+
+    public Point3d getCoordinate3() {
+        return coordinate3;
+    }
+
+
+    public void setCoordinate3(Point3d coordinate3) {
+        this.coordinate3 = coordinate3;
+    }
+
 }
