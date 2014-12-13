253f0b76b721c7582522db760d5ed7d2210f7db2
Samuel von Baussnern <samuel.vonbaussnern@gmail.com>
2014-12-13 11:56:52 +0100

    first steps

--- FractalLandscapeGenerator/src/algorithm/DiamondSquare.java
+++ FractalLandscapeGenerator/src/algorithm/DiamondSquare.java
@@ -1,5 +1,20 @@
 package algorithm;
 
-public class DiamondSquare {
 
+public class DiamondSquare {
+	public static void applyDiamondSquare(double[][] points, double roughness) {
+		for (int i = points.length; i > 0; --i) {
+			diamondStep(points, roughness, i);
+			squareStep(points, roughness, i);
+			
+			roughness /= 2;
+		}
+	}
+	
+	private static void diamondStep(double[][] points, double roughness, int length) {
+		
+	}
+	
+	private static void squareStep(double[][] points, double roughness, int length) {
+	}
 }
